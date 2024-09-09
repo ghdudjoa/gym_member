9631f93d28194abf6f2a5d212fcbdcdc585e3351
ghdud1038 <ghdud1038@naver.com>
2024-09-09 13:14:17 +0900

    Gym_MemberDao입니당

---
+++ src/gym_member/Gym_MemberDao.java
@@ -0,0 +1,17 @@
+package gym_member;
+
+import java.util.List;
+
+public interface Gym_MemberDao {
+	
+	boolean insert(Gym_Member member);
+	
+	List<Gym_Member> findAll();
+	
+	Gym_Member findByMember_Num(int member_num);
+	
+	boolean update (Gym_Member member);
+	
+	boolean deleteByMember_Num(int member_num);
+	
+}
